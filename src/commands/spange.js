7db40f08e11754380303a9d743ab8bf343bc8fe4
Alex Taxiera <alex.taxiera@onesky.com>
2019-10-21 09:39:46 -0400

    add safety for messages full of non letters

--- src/commands/spange.js
+++ src/commands/spange.js
@@ -7,32 +7,43 @@ module.exports = new Command({
     deleteResponse: false
   },
   run: async ({ params }) => {
-    const fullParam = params.join(' ').toLowerCase()
-    const indices = []
-
-    let rng
-    let charCode = ''
-    for (let i = 0; i < (fullParam.length * 0.33); i++) {
-      do {
-        rng = Math.floor(Math.random() * (fullParam.length - 1 - 0 + 1)) + 0
-        charCode = fullParam[rng].charCodeAt()
-      } while (badChar(indices, charCode, rng))
-
-      indices.push(rng)
-    }
-
-    let str = ''
-    for (let i = 0; i < fullParam.length; i++) {
-      if (indices.includes(i)) {
-        str += String.fromCharCode(fullParam[i].charCodeAt() - 32)
-      } else {
-        str += fullParam[i]
+    return params.map((param) => {
+      param = param.toLowerCase()
+      if (param.split('').every((ch) => badChar(ch.charCodeAt()))) {
+        return param
       }
-    }
-    return str
+      const indices = []
+
+      let rng
+      let charCode = ''
+      for (let i = 0; i < (param.length * 0.33); i++) {
+        do {
+          rng = Math.floor(Math.random() * (param.length - 1 - 0 + 1)) + 0
+          charCode = param[rng].charCodeAt()
+        } while (badChar(charCode) || tooClose(indices, rng))
+
+        indices.push(rng)
+      }
+
+      let str = ''
+      for (let i = 0; i < param.length; i++) {
+        if (indices.includes(i)) {
+          str += String.fromCharCode(param[i].charCodeAt() - 32)
+        } else {
+          str += param[i]
+        }
+      }
+      return str
+    }).join(' ')
   }
 })
 
 function badChar (indices, charCode, rng) {
-  return charCode > 122 || charCode < 97 || indices.includes(rng) || indices.includes(rng + 1) || indices.includes(rng - 1)
+  return charCode > 122 || charCode < 97
+}
+
+function tooClose (indices, index) {
+  return indices.includes(index) ||
+  indices.includes(index + 1) ||
+  indices.includes(index - 1)
 }
