362b2f360dd77af279b48c057968df6df56d101c
Alex Taxiera <alex.taxiera@onesky.com>
2020-08-09 19:18:23 -0400

    fix linting

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,10 +1,13 @@
 import {
+  Message
+} from 'eris'
+import {
   DiscordEvent
 } from 'eris-boiler'
 
 export default new DiscordEvent({
   name: 'messageCreate',
-  run: (_, msg): void => {
+  run: (_, msg: Message): void => {
     if (msg.content.toLowerCase() === 'ayy') {
       msg.channel.createMessage('lmao')
     }
