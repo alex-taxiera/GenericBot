9e41d3d2ec9ee996568fbf6c2cb8c34d525a2db3
Alex Taxiera <alex.taxiera@gmail.com>
2022-06-27 22:05:50 -0400

    fix linting

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,5 @@ import { client } from '@client'
 
 client.commands.add(join(__dirname, 'commands'))
 client.events.add(join(__dirname, 'events'))
+// eslint-disable-next-line no-console
 client.connect().catch(console.error)
