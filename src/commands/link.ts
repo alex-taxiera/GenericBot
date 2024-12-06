1648e851079eb0f279c5264ff81d2e4ad7309c61
Alex Taxiera <alex.taxiera@gmail.com>
2024-12-06 10:39:59 -0500

    update link command and permissions

--- src/commands/link.ts
+++ src/commands/link.ts
@@ -1,20 +1,21 @@
 import { TopLevelCommand } from '@hephaestus/eris'
-import config from 'config'
 
 const command: TopLevelCommand = {
   type: 1,
-  guildId: config.get('inviteOptions.guildId'),
   name: 'link',
   description: 'Generate a single use, 24hr invite link for the server',
   action: async (interaction, client) => {
     const invite = await client.createChannelInvite(
-      config.get('inviteOptions.channelId'),
+      interaction.channel.id,
       {
         maxAge: 24 * 60 * 60,
         maxUses: 1,
       }, `Created for ${interaction.member?.id ?? ''}`)
 
-    void interaction.createMessage(`discord.gg/${invite.code}`)
+    void interaction.createMessage({
+      content: `discord.gg/${invite.code}`,
+      flags: 64,
+    })
   },
 }
 
--- src/modules/client.ts
+++ src/modules/client.ts
@@ -4,7 +4,6 @@ import { Forge } from '@hephaestus/eris'
 export const client = new Forge(config.get('DISCORD_TOKEN'), {
   intents: [
     'guilds',
-    'guildMembers',
     'guildMessages',
   ],
   restMode: true,
